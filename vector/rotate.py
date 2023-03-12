03bed40b2c486a8ed097419fb01aad2381e155cd
李芳毅 <leefyi@126.com>
2023-03-12 14:33:33 +0800

    三角学；

---
+++ vector/rotate.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin python
+# coding=utf-8
+# @Time    : 2023/3/12 14:21
+# @Author  : lifangyi
+# @File    : rotate.py
+# @Software: PyCharm
+
+
+# 实现向量的逆时针旋转函数
+# 接受笛卡尔坐标集合和一个固定的弧度
+
+from math import sin, cos, atan2, pow, sqrt
+
+def rotate(vectors, arc):
+    nv = []
+    for vector in vectors:
+        x, y = vector[0], vector[1]
+        l = sqrt(pow(x, 2) + pow(y,2))
+        ac = atan2(y, x)
+        aca = ac + arc
+        nx = sin(aca)*l
+        ny = cos(aca)*l
+        nv.append((nx,ny))
+    return nv
+
